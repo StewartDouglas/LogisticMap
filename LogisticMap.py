a6bd637a12b2b0113c67ef8c5022592d1292d8b4
Stewart Douglas <stewart@Stewarts-MacBook-Air.local>
2013-11-26 17:29:32 +0000

    Updated with comments

--- LogisticMap.py
+++ LogisticMap.py
@@ -1,26 +1,38 @@
-import numpy
-import matplotlib 
-import matplotlib.pyplot as plt
-import scipy
+import matplotlib as plt
+import pylab
 
+# Used for pertubation
 eps = 0.01
+# u is the dependent variable here
 u = 0.0
-r = 2.9
-dr = 0.001
+# Small perturbation in u
 u = u + eps
+
+# r is plotted on the x-axis. Different
+# values of r produce very different behaviour.
+# In population dynamics r represents the
+# combined rate for reproduction and starvation
+r = 0.0
+dr = 0.005
+
+# Stored the x-axis values in r_list,
+# and the y-axis values in u_list
 r_list = []
 u_list = []
 
 while r < 4.0:
-    for i in range(1,501):
+    # Throw away the 1st 200 iterations so that we
+    # converge to the equilibria
+    for i in range(1,200):
         u = r*u*(1.0 - u)
-    for i in range(1,100):
+    for i in range(1,500):
         u = r*u*(1.0 - u)
         r_list.append(r)
         u_list.append(u)
     r = r+dr
     u = u+eps
 
-plt.pylot.scatter(r_list,u_list, s=1, facecolor='0.1', lw = 0)
-#plt.pyplot.axis([2.9,4.0,0,1])
-#plt.pyplot.show()
+# Plot the output
+plt.pyplot.scatter(r_list,u_list, s=1, facecolor='0.1', lw = 0)
+plt.pyplot.axis([1.0,4.0,0,1.0])
+plt.pyplot.show()
